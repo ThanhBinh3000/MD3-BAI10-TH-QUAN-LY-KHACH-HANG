9ad24ffdd2f077316a3f15fc0ab25118e8d2d607
unknown <binhk4a@gmail.com>
2022-03-04 14:52:23 +0700

    B10

---
+++ src/main/java/com/codegym/model/Customer.java
@@ -0,0 +1,50 @@
+package com.codegym.model;
+
+public class Customer {
+    private  int id; // id của khách hàng
+    private String name; // Tên của khách hàng
+    private String email; // Email của khách hàng
+    private String address; // địa chỉ của khách hàng
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
